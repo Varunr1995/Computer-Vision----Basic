c4d6091adadc0da77300f24eb46d000850828e69
Varunr1995 <59080666+Varunr1995@users.noreply.github.com>
2020-09-04 14:04:53 +0530

    Add files via upload

---
+++ Cv Object Detection -- Basic/detection.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  4 13:27:45 2020
+
+@author: varun
+"""
+
+
+import cv2 
+
+
+faceCascade = cv2.CascadeClassifier(cv2.haarcascades +"haarcascade_fullbody.xml")
+
+video_capture = cv2.VideoCapture("C:/Users/varun/Downloads/Full-Body-Detection-master/Subway.mp4")
+while True:
+    # Capture frame-by-frame
+    ret, frames = video_capture.read()
+    gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=1,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frames, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    # Display the resulting frame
+    cv2.imshow('Video', frames)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
